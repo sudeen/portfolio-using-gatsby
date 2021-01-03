f386fc15e3682a3688dafdbb53d90c2e21dd17b9
sudeen <ranjitkarsudeen14@gmail.com>
2021-01-03 13:32:10 +1100

    adding small info and my hero img

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,7 @@ import SocialLinks from "../constants/socialLinks"
 
 const query = graphql`
   {
-    file(relativePath: { eq: "hero-img.png" }) {
+    file(relativePath: { eq: "hero-sudin.jpg" }) {
       childImageSharp {
         fluid {
           ...GatsbyImageSharpFluid
@@ -16,7 +16,29 @@ const query = graphql`
   }
 `
 const Hero = () => {
-  return <h2></h2>
+  const {
+    file: {
+      childImageSharp: { fluid },
+    },
+  } = useStaticQuery(query)
+  return (
+    <header className="hero">
+      <div className="section-center hero-center">
+        <article className="hero-info">
+          <div>
+            <div className="underline"></div>
+            <h1>I'm Sudin</h1>
+            <h4>Freelance Full Stack Developer</h4>
+            <Link to="/contact" className="btn">
+              contact me
+            </Link>
+            <SocialLinks />
+          </div>
+        </article>
+        <Image fluid={fluid} className="hero-img" />
+      </div>
+    </header>
+  )
 }
 
 export default Hero
